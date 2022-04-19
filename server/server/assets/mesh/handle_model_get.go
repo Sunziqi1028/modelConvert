3d6e7a968e9fa627660a86906a0000d01c4c1a86
sunziqi <sunziqi@miaoxiang.co>
2022-04-19 14:15:23 +0800

    添加获取modelId

---
+++ server/server/assets/mesh/handle_model_get.go
@@ -0,0 +1,36 @@
+package mesh
+
+import (
+	"fmt"
+	"net/http"
+
+	"shadoweditor/helper"
+	"shadoweditor/server"
+)
+
+func init() {
+	server.Handle(http.MethodGet, "/api/Mesh/modelId", ModelId, server.GetModelId)
+}
+
+// List returns the mesh list.
+func ModelId(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	modelID := query.Get("modelID")
+	list := Model{}
+	mysql := server.Mysql()
+	err := mysql.Table(server.MeshCollectionName).Where("model_id = ?", modelID).First(&list).Error
+	if err != nil {
+		fmt.Println(err)
+		helper.WriteJSON(w, server.Result{
+			Code: 300,
+			Msg:  "查询失败",
+		})
+		return
+	}
+	list.URL = URL + list.URL
+	helper.WriteJSON(w, server.Result{
+		Code: 200,
+		Msg:  "Get Successfully!",
+		Data: list,
+	})
+}
