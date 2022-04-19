3834e44973f442335d1a90320492fff7bc5b14a9
Sunziqi1028 <58042498+Sunziqi1028@users.noreply.github.com>
2022-04-19 17:47:50 +0800

    Add files via upload

---
+++ server/server/assets/mesh/handle_model_get.go
@@ -0,0 +1,41 @@
+package mesh
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"shadoweditor/helper"
+	"shadoweditor/server"
+)
+
+func init() {
+	server.Handle(http.MethodGet, "/api/Mesh/modelId", ModelId, server.GetModelId)
+}
+
+// List returns the mesh list.
+func ModelId(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	modelID := query.Get("modelID")
+	modelId, err := strconv.Atoi(modelID)
+	if err != nil {
+		fmt.Println(err)
+	}
+	list := Model{}
+	mysql := server.Mysql()
+	err = mysql.Table(server.MeshCollectionName).Where("model_id = ?", modelId).First(&list).Error
+	if err != nil {
+		fmt.Println(err)
+		helper.WriteJSON(w, server.Result{
+			Code: 300,
+			Msg:  "查询失败",
+		})
+		return
+	}
+	list.URL = URL + list.URL
+	helper.WriteJSON(w, server.Result{
+		Code: 200,
+		Msg:  "Get Successfully!",
+		Data: list,
+	})
+}
